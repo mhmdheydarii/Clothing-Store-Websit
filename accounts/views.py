d174a7796f73f02bbb362a75cc8d57ca4ea336d5
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-07-01 01:43:11 -0700

    create accounts models

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
 
+from .models import User, Profile
 # Create your views here.
+
+# class RegisterView(SuccessMessageMixin ,CreateView):
+
+#     form_class = RegisterForm
+#     template_name = ""
+#     success_url = reverse_lazy("")
--- core/settings.py
+++ core/settings.py
@@ -134,3 +134,6 @@ MEDIA_ROOT = 'media/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# User model
+AUTH_USER_MODEL = 'accounts.User'
