61769b7c5ba30020417bc20e4d074159524c7995
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-07-20 03:58:16 -0700

    make better some page like failed after payment

--- dashboard/customer/urls.py
+++ dashboard/customer/urls.py
@@ -5,6 +5,5 @@ app_name = "customer"
 
 urlpatterns = [
     path("profile/", views.CustomerProfileView.as_view(), name="profile"),
-    path("address/", views.CustomerAddressView.as_view(), name="address"),
     path("order/", views.CustomerOrderView.as_view(), name="order"),
 ]
--- dashboard/customer/views.py
+++ dashboard/customer/views.py
@@ -1,13 +1,14 @@
-from django.views.generic import TemplateView, FormView, UpdateView
+from django.views.generic import TemplateView, FormView, UpdateView, ListView
 from dashboard.permissions import UserCustomerPermission
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from .forms import CustomerProfileForm
 from accounts.models import Profile
+from order.models import OrderModel
 
-
-class CustomerProfileView(UserCustomerPermission, SuccessMessageMixin ,UpdateView):
+class CustomerProfileView(UserCustomerPermission, LoginRequiredMixin, SuccessMessageMixin ,UpdateView):
     form_class = CustomerProfileForm
     template_name = "dashboard/customer/profile.html"
     success_message = "اطلاعات شما با موفقیت بروزرسانی شد."
@@ -20,8 +21,13 @@ class CustomerProfileView(UserCustomerPermission, SuccessMessageMixin ,UpdateVie
         messages.error(self.request, "مشکلی در بروزرسانی اطلاعات بوجود آمد.")
         return super().form_invalid(form)
 
-class CustomerAddressView(TemplateView):
-    template_name = "dashboard/customer/address.html"
 
-class CustomerOrderView(TemplateView):
+class CustomerOrderView(UserCustomerPermission, LoginRequiredMixin, ListView):
     template_name = "dashboard/customer/order.html"
+    
+    queryset = OrderModel.objects.filter(status=OrderModel.OrderStatusTypeModel.PAID)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["orders"] = OrderModel.objects.filter(status=OrderModel.OrderStatusTypeModel.PAID)
+        return context
