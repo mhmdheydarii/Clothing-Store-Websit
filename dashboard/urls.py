e15e41619f3077a42db23630770e3e3b28fc09b1
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-09-01 10:42:22 -0700

    Update urls.py
    Update : Dashboard urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,6 +6,6 @@ app_name = "dashboard"
 urlpatterns = [
     path("profile/", views.DashboardView.as_view(), name="profile"),
 
-    # path("admin/", include("admin.urls")),
+    #path("admin/", include("admin.urls")),
     path("customer/", include("dashboard.customer.urls")),
 ]
